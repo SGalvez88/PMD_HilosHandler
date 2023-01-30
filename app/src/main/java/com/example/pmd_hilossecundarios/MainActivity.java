8b488e29159cdc498970d246e28e5a8f75914dbb
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-01-30 09:35:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pmd_hilossecundarios/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pmd_hilossecundarios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonDescarga = (Button) findViewById(R.id.bDescarga);
+        botonDescarga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                
+
+
+
+            }
+        });
+
+    }
+}
